cffc2616f2612f25ca2414e503eec863f7d8f3cc
Tayo Olowu <olowu.otayo@gmail.com>
2023-10-05 09:09:24 -0700

    no comment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define BY "byte(s)"
 
 /**
  * main - Betty, leave me alone coding isn't easy.
@@ -6,13 +7,13 @@
  */
 int main(void)
 {
-	char fmt[] = "%s%d%s\n";
+	char fmt[] = "%s %d %s\n";
 
-	printf(fmt, "Size of a char: ", sizeof(char), " byte(s)");
-	printf(fmt, "Size of an int: ", sizeof(int), " bytes(s)");
-	printf(fmt, "Size of a long int: ", sizeof(long int), " byte(s)");
-	printf(fmt, "Size of a long long int: ", sizeof(long long int), " byte(s)");
-	printf("%s%d%s", "Size of a float: ", sizeof(float), " byte(s)");
+	printf(fmt, "Size of a char:", sizeof(char), BY);
+	printf(fmt, "Size of an int:", sizeof(int), BY);
+	printf(fmt, "Size of a long int:", sizeof(long int), BY);
+	printf(fmt, "Size of a long long int:", sizeof(long long int), BY);
+	printf(fmt, "Size of a float:", sizeof(float), BY);
 
 	return (0);
 }
