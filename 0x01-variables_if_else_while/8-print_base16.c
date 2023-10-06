2019bae74c9ee2f9e9491095ff52c608def61c2a
Tayo Olowu <olowu.otayo@gmail.com>
2023-10-06 04:58:52 -0700

    Exercises 0-8 done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - prints hex digits in a fake way.
+ * Return: 0 means success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0' ; ch <= '9' ; ++ch)
+		putchar(ch);
+	for (ch = 'a' ; ch <= 'f' ; ++ch)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
