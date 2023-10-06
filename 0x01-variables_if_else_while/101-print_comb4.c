ccdaedc145c7ca114dacc526755a0b24792728a9
Tayo Olowu <olowu.otayo@gmail.com>
2023-10-06 05:23:52 -0700

    Started the optional ones

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+#define ZERO 48
+#define NINE 57
+
+/**
+ * main - prints the smaller two-digit numbers as chars.
+ * Return: 0 for success 1 for failure
+ */
+int main(void)
+{
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#define ZERO 48
+#define NINE 57
+
+/**
+ * main - prints 0 to 9 in a weird way
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = ZERO ; n <= NINE ; ++n)
+	{
+		if (n == NINE)
+			putchar(n);
+		else
+		{
+			putchar(n);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
