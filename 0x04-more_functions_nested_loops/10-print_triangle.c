04e6def1e0a31edb9499f07e6f620bb7a6946b54
tdo-olowu <olowu.otayo@gmail.com>
2023-10-12 15:26:29 -0700

    fixed number ten

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,13 +11,13 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 1 ; i <= size ; ++i)
+		for (i = size - 1 ; i >= 0 ; --i)
 		{
-			for (j = 1 ; j < size - i ; ++j)
+			for (j = 0 ; j < i ; ++j)
 			{
 				_putchar(' ');
 			}
-			for (j = 0 ; j < i ; ++j)
+			for (j = 0 ; j < size - i ; ++j)
 			{
 				_putchar('#');
 			}
