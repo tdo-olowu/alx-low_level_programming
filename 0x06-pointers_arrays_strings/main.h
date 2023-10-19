3a38d8f87683c0956eb9cd95b1bb48e2a0aa1e14
tdo-olowu <olowu.otayo@gmail.com>
2023-10-19 08:51:49 -0700

    Files added

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+char *_strcat(char*, char*);
+char *_strncat(char*, char*, int);
+char *_strncpy(char*, char*, int);
+int _strcmp(char*, char*);
+void reverse_array(int, int);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *leet(char*);
+
+
+#endif /* MAIN_H */
