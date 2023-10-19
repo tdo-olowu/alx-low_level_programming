ea4acd3e8b8d6907fe6f2bfed0ff8f6fab44ede2
tdo-olowu <olowu.otayo@gmail.com>
2023-10-19 16:48:32 -0700

    updates

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; dest[i] != '\0' ; ++i)
 	{
 	}
-	for (j = 0 ; (j < n) && (str[j] != '\0') ; ++j)
+	for (j = 0 ; (j < n) && (src[j] != '\0') ; ++j)
 	{
 		dest[i + j] = src[j];
 	}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ char *_strcat(char*, char*);
 char *_strncat(char*, char*, int);
 char *_strncpy(char*, char*, int);
 int _strcmp(char*, char*);
-void reverse_array(int, int);
+void reverse_array(int*, int);
 char *string_toupper(char*);
 char *cap_string(char*);
 char *leet(char*);
