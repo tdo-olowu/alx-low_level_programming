beecd3b808e6f5e66f83a0ae3d1f1cffddde07aa
tdo-olowu <olowu.otayo@gmail.com>
2023-10-19 17:37:01 -0700

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
 	n = ((n <= 0) ? (-1 * n) : n);
-	for (i = 0 ; (dest[i] != '\0') && (i < n) ; ++i)
+	for (i = 0 ; (src[i] != '\0') && (i < n) ; ++i)
 	{
-		if (src[i] != '\0')
-			dest[i] = src[i];
-		else
-			break;
+		dest[i] = src[i];
 	}
 	while (i < n)
 	{
-		dest[i] == '\0';
+		dest[i] = '\0';
 		++i;
 	}
-	return (dest);
+	return dest;
 }
