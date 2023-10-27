b583c37e96515c4228d55fc3406dee774f715493
tdo-olowu <olowu.otayo@gmail.com>
2023-10-27 14:58:25 -0700

    creating static

---
+++ 0x09-static_libraries/0x09_junkyard/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - measures a string by its number of characters.
+ * @s: the string in question, stored in an array.
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0 ; s[len] != '\0' ; ++len)
+	{
+	}
+	return (len);
+}
