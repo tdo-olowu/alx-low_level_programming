875385850ef16e4da0ead940798b824da5b43eb9
tdo-olowu <olowu.otayo@gmail.com>
2023-10-30 14:10:20 -0700

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,15 +11,13 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *cat;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
 	for (i = 0 ; s1[i] != '\0' ; ++i)
 		;
 	for (j = 0 ; s2[j] != '\0' ; ++j)
 		;
-
-	if (s1 == NULL)
-		i = 0;
-	if (s2 == NULL)
-		j = 0;
 
 	cat = malloc((i + j + 1) * sizeof(char));
 	if (cat != NULL)
