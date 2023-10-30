a729862868956f3ebd7e1df8e081be596d2bd555
tdo-olowu <olowu.otayo@gmail.com>
2023-10-30 14:19:48 -0700

    fixed tricky NULL ptr bug

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,17 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *cat;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
-	for (i = 0 ; s1[i] != '\0' ; ++i)
+	for (i = 0 ; s1[i] != '\0' && s1 != NULL ; ++i)
 		;
-	for (j = 0 ; s2[j] != '\0' ; ++j)
+	for (j = 0 ; s2[j] != '\0' && s2 != NULL ; ++j)
 		;
 
 	cat = malloc((i + j + 1) * sizeof(char));
 	if (cat != NULL)
 	{
-		for (k = 0 ; k < i ; ++k)
+		for (k = 0 ; k < i && s1 != NULL ; ++k)
 			cat[k] = s1[k];
-		for (k = 0 ; k < j ; ++k)
+		for (k = 0 ; k < j && s2 != NULL; ++k)
 			cat[k + i] = s2[k];
 		cat[i + j] = '\0';
 		return (cat);
