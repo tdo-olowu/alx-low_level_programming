73fc1a45b4d167070f0d1059d96a8209ef1dd426
tdo-olowu <olowu.otayo@gmail.com>
2023-11-02 08:52:37 -0700

    updated

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int _putchar(char);
-void *malloc_checked(unsigned int);
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char*, char*, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
