5377f4f161277b666e72628f3d46eff5693aa141
tdo-olowu <olowu.otayo@gmail.com>
2023-11-06 17:21:41 -0800

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,31 +11,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog;
+	dog_t *dog;
 	int l_name, l_owner;
 
 	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
 	for (l_name = 0 ; name[l_name] != '\0' ; ++l_name)
 		;
 	for (l_owner = 0 ; owner[l_owner] != '\0' ; ++l_owner)
 		;
 
-	dog.name = malloc((l_name + 1) * sizeof(char));
-	dog.age = malloc(sizeof(float));
-	dog.owner = malloc((l_owner + 1) * sizeof(char));
-	if (dog.name != NULL)
+	dog->name = malloc((l_name + 1) * sizeof(char));
+	dog->age = age;
+	dog->owner = malloc((l_owner + 1) * sizeof(char));
+	if (dog->name != NULL)
 	{
 		for ( ; l_name >= 0 ; --l_name)
-			(dog.name)[l_name] = name[l_name];
+			(dog->name)[l_name] = name[l_name];
 	}
-	if (dog.age != NULL)
-	{
-		dog.age = age;
-	}
-	if (dog.owner != NULL)
+	if (dog->owner != NULL)
 	{
 		for ( ; l_owner >= 0 ; --l_owner)
-			(dog.owner)[l_owner] = owner[l_owner];
+			(dog->owner)[l_owner] = owner[l_owner];
 	}
 
 	return (dog);
