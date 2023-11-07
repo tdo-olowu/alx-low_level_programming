5869e2b803ee3c2d6b6172270f30ffe2fef34d29
tdo-olowu <olowu.otayo@gmail.com>
2023-11-06 16:45:26 -0800

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,6 @@
 #define DOG_H
 
 #include <stdio.h>
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 
 /**
@@ -19,7 +15,7 @@ void free_dog(dog_t *d);
 struct dog
 {
 	char *name;
-	char age;
+	float age;
 	char *owner;
 };
 
