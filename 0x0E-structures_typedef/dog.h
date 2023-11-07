fdbcdc16b176162557f6903d677907e4d6d10a1d
tdo-olowu <olowu.otayo@gmail.com>
2023-11-06 16:35:06 -0800

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
 /**
- * dog - simulates a dog very cheaply.
+ * struct dog - simulates a dog very cheaply.
  * @name: the chien's name
  * @age: the inu's age
  * @owner: aja's owner
+ *
+ * Description: longer description
  */
 struct dog
 {
