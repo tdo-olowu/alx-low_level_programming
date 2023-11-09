4cff70c93bf56b823cf3b7c8a6ed152b82ca994d
tdo-olowu <olowu.otayo@gmail.com>
2023-11-08 17:31:24 -0800

    updated

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,13 +11,12 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i = 0;
 	op_t ops[] = {  {"+", op_add}, {"-", op_sub},
 			{"*", op_mul}, {"/", op_div},
 			{"%", op_mod}, {NULL, NULL}
 			};
-    	int i;
 
-	i = 0;
 	while (i < 5)
 	{
 		if ((s != NULL) && (*s == *(ops[i].op)))
