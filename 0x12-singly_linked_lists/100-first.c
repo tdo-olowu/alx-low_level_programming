59a14b4409b93b2853b5748a63b82457888b4f0c
tdo-olowu <olowu.otayo@gmail.com>
2023-11-16 11:39:18 -0800

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-void tortoise(void) __attribute__ ((constructor))
+__attribute__((constructor)) void tortoise(void) 
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
