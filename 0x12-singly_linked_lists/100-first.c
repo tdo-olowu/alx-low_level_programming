7e6545fe24d19a1ab4c3af64aa2a8d6e1bd19e17
tdo-olowu <olowu.otayo@gmail.com>
2023-11-16 11:47:22 -0800

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * tortoise - a function which runs prior to main.
  * Return: none.
  */
-void tortoise(void) 
+void tortoise(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
