83838ee326fe1974a5ed18bb4a044601bd119c19
tdo-olowu <olowu.otayo@gmail.com>
2023-11-23 13:45:03 -0800

    updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,5 +8,19 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	s;
+	unsigned long int bit_seek;
+	unsigned int i;
+
+	if ((n == 0) && (index == 0))
+		return (0);
+	if ((n > 0) && ((n >> index) > 0))
+	{
+		for (bit_seek = 1, i = 0 ; i < index ; ++i)
+			bit_seek <<= 1;
+		if ((bit_seek & n) != 0)
+			return (1);
+		return (0);
+	}
+
+	return (-1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,3 +7,22 @@
  * Return: 1 if it worked, -1 if error.
  */
 int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int toggle_on;
+	unsigned int i;
+
+	if ((*n >> index) > 0)
+	{
+		for (toggle_on = 1, i = 0 ; i < index ; ++i)
+			toggle_on <<= 1;
+		*n = *n | toggle_on;
+		return (1);
+	}
+	if ((*n == 0) && (index == 0))
+	{
+		*n = *n | 1;
+		return (1);
+	}
+
+	return (-1);
+}
