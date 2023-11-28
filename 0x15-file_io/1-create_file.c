7fca886e12eb63fcba107be13245e69bb2b01bf7
tdo-olowu <olowu.otayo@gmail.com>
2023-11-28 13:19:37 -0800

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#define OPEN_FLAGS O_CREAT | O_WRONLY | O_TRUNC
-#define MODE_FLAGS S_IRUSR | S_IWUSR
 
 /**
  * create_file - creates a file.
@@ -16,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_d = open(filename, OPEN_FLAGS, MODE_FLAGS);
+	file_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_d < 0)
 		return (-1);
 
