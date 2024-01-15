2136fa6cc35b9c63db2eff83eede65507872d2ae
Tayo Olowu <olowu.otayo@gmail.com>
2024-01-15 12:26:55 +0100

    dynamic libraries updates added

---
+++ 0x18-dynamic_libraries/0x18-junkyard/operations/operations.c
@@ -0,0 +1,56 @@
+#include "ops.h"
+
+/**
+ * add - adds two numbers
+ * @a: a
+ * @b: b
+ * Return: integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - the difference of two numbers
+ * @a: a
+ * @b: b
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - the product of two numbers
+ * @a: a
+ * @b: b
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - the quotient of two numbers
+ * @a: a
+ * @b: b
+ * Return: integer part of a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - the remainder when a is divided by b
+ * @a: a
+ * @b: b
+ * Return: an integer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/0x18-junkyard/operations/ops.h
@@ -0,0 +1,11 @@
+#ifndef OPS_H
+#define OPS_H
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+
+#endif /* OPS_H */
