8090480663cc9cd7a33482d9cd1d94f5baffb9b5
Tayo Olowu <olowu.otayo@gmail.com>
2024-05-08 22:18:57 +0100

    files updated

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,1 +1,28 @@
 #include "search_algos.h"
+
+
+/**
+ * linear_search - searches for a value in array
+ * using linear search algorithm
+ * @array: pointer to the first element of the array to search
+ * @size: the number of elements in the array
+ * @value: the value to search for
+ *
+ * Return: the first index where value is located, or -1 if error
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+	char *fmt = "Value checked array[%lu] = [%d]\n";
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0 ; i < size ; ++i)
+	{
+		printf(fmt, i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,1 +1,16 @@
 #include "search_algos.h"
+
+/**
+ * binary_search - searches list for value using binary search
+ * it is assumed that the array is sorted in ascending order.
+ * and the value appears only once in the array
+ *
+ * @array: pointer to the head of the array
+ * @size: the number of elements in the array
+ * @value: the value to search for.
+ * Return: index of the value if it's there, -1 if not
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (-1);
+}
