82b66e0bfe9cbc974fef6cad71960e65e0d9e988
Tayo Olowu <olowu.otayo@gmail.com>
2024-05-09 01:00:58 +0100

    files updated

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,5 +12,34 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
+	size_t i, l, jump;
+	char *fmt = "Value checked array[%lu] = [%d]\n";
+	char *fmtr = "Values found between indexes [%lu] and [%lu]\n";
+
+	if (array == NULL)
+		return (-1);
+
+	i = 0;
+	jump = sqrt(size);
+	while (i < size)
+	{
+		printf(fmt, i, array[i]);
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+		{
+			printf(fmtr, i - jump, i);
+			break;
+		}
+		i += jump;
+	}
+	i -= jump;
+	/* now perform a linear search */
+	for (l = i ; (l < size) && (l <= i + jump) ; ++l)
+	{
+		printf(fmt, l, array[i]);
+		if (array[l] == value)
+			return (l);
+	}
 	return (-1);
 }
